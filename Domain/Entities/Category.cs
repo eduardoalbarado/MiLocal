4126c6021abdbffa82a08eddce0993828b233e51
Eduardo <eduardoalbarado@gmail.com>
2023-04-25 10:01:01 -0300

    Add Infrastructure Project and DbContext

--- Domain/Entities/Category.cs
+++ Domain/Entities/Category.cs
@@ -6,5 +6,5 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public virtual ICollection<Product>? Products { get; set; }
+    public ICollection<ProductCategory>? ProductCategories { get; set; }
 }
