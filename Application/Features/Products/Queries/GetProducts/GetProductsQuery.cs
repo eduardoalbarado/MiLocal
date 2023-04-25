e079061176a19b286dfd78f52b7e8998874841b7
Eduardo <eduardoalbarado@gmail.com>
2023-04-25 17:56:45 -0300

    Add GetProducts Feature

---
+++ Application/Features/Products/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,36 @@
+﻿using Application.Common.Models;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Features.Products.Queries.GetProducts;
+
+public class GetProductsQuery : ProductDto, IRequest<PaginatedList<ProductDto>>
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public bool Enabled { get; set; }
+}
+
+public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, PaginatedList<ProductDto>>
+{
+    private readonly IRepositoryAsync<Product> _repositoryAsync;
+    private readonly IMapper _mapper;
+
+    public GetProductsQueryHandler(IMapper mapper, IRepositoryAsync<Product> repositoryAsync)
+    {
+        _mapper = mapper;
+        _repositoryAsync = repositoryAsync;
+    }
+
+    public async Task<PaginatedList<ProductDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+    {
+        var spec = new ProductPagedSpecifications(request.PageNumber, request.PageSize, request.Enabled);
+        var products = await _repositoryAsync.ListAsync(spec, cancellationToken);
+        var productsCount = await _repositoryAsync.CountAsync(spec, cancellationToken);
+        var result = _mapper.Map<PaginatedList<ProductDto>>(products);
+
+        return new List<ProductDto>(result, productsCount, request.PageNumber, request.PageSize);
+    }
+}
---
+++ Application/Features/Products/Queries/GetProducts/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.Features.Products.Queries.GetProducts
+{
+    public class ProductDto : IMapFrom<Product>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<Product, ProductDto>();
+        }
+    }
+}
