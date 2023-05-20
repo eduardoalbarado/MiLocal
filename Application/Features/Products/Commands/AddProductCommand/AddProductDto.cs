54b6ee4e23cf99d9b7e034bd1378886fb7595faf
Eduardo <eduardoalbarado@gmail.com>
2023-05-20 19:56:33 -0300

    Add UpdateProductCommandValidator

--- Application/Features/Products/Commands/AddProductCommand/AddProductDto.cs
+++ Application/Features/Products/Commands/AddProductCommand/AddProductDto.cs
@@ -2,10 +2,13 @@
 {
     public class AddProductDto
     {
-        public string Description { get; set; }
-        public bool Enabled { get; set; }
         public string Name { get; set; }
-        public decimal Price { get; set; }
         public string ShortName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal PriceInDollars { get; set; }
+        public decimal Cost { get; set; }
+        public decimal CostInDollars { get; set; }
+        public bool Kit { get; set; }
     }
 }
---
+++ Application/Features/Products/Commands/AddProductCommand/UpdateProductCommandValidator.cs
@@ -0,0 +1,33 @@
+using FluentValidation;
+
+namespace Application.Features.Products.Commands.AddProduct
+{
+    public class UpdateProductCommandValidator : AbstractValidator<AddProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty().WithMessage("Name is required.")
+                .MaximumLength(100).WithMessage("Name must not exceed 100 characters.");
+
+            RuleFor(p => p.ShortName)
+                .NotEmpty().WithMessage("ShortName is required.")
+                .MaximumLength(50).WithMessage("ShortName must not exceed 50 characters.");
+
+            RuleFor(p => p.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+            RuleFor(p => p.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0.");
+
+            RuleFor(p => p.PriceInDollars)
+                .GreaterThanOrEqualTo(0).WithMessage("PriceInDollars must be greater than or equal to 0.");
+
+            RuleFor(p => p.Cost)
+                .GreaterThanOrEqualTo(0).WithMessage("Cost must be greater than or equal to 0.");
+
+            RuleFor(p => p.CostInDollars)
+                .GreaterThanOrEqualTo(0).WithMessage("CostInDollars must be greater than or equal to 0.");
+        }
+    }
+}
