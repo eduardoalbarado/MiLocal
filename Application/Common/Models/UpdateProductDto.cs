5b4247f93c4f5588db49f60d7d64cee8b5305c6f
Eduardo <eduardoalbarado@gmail.com>
2023-05-20 15:31:17 -0300

    Add UpdateProductCommand

---
+++ Application/Common/Models/UpdateProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Common.Models;
+public class UpdateProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string ShortName { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public decimal PriceInDollars { get; set; }
+    public decimal Cost { get; set; }
+    public decimal CostInDollars { get; set; }
+    public bool Enabled { get; set; }
+    public bool Kit { get; set; }
+}
---
+++ Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entityName, int entityId)
+            : base($"The entity '{entityName}' with id '{entityId}' was not found.")
+        {
+        }
+    }
+}
