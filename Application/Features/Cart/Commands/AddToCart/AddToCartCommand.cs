ee264470dfeb0aa662a9cd5a887c28ffcfd7f5b0
Eduardo <eduardoalbarado@gmail.com>
2023-05-28 00:41:11 -0300

    Refactor AddToCartCommandHandler and AddToCartCommand
    - Removed unused UserId property from AddToCartCommand.
    - Added IUserContextService dependency to AddToCartCommandHandler.
    - Extracted GetProductById, GetOrCreateCart, and CreateCartItem methods in AddToCartCommandHandler for better code organization.
    - Updated Handle method in AddToCartCommandHandler to utilize the extracted methods.
    - Improved code readability and maintainability.

--- Application/Features/Cart/Commands/AddToCart/AddToCartCommand.cs
+++ Application/Features/Cart/Commands/AddToCart/AddToCartCommand.cs
@@ -1,13 +1,12 @@
 using Application.Exceptions;
+using Application.Interfaces;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Carts.Commands.AddToCart;
-
 public class AddToCartCommand : IRequest<int>
 {
-    public Guid UserId { get; set; }
     public int ProductId { get; set; }
     public int Quantity { get; set; }
 }
@@ -16,50 +15,68 @@ public class AddToCartCommandHandler : IRequestHandler<AddToCartCommand, int>
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly IUserContextService _userContextService;
 
-    public AddToCartCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public AddToCartCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, IUserContextService userContextService)
     {
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _userContextService = userContextService;
     }
 
     public async Task<int> Handle(AddToCartCommand request, CancellationToken cancellationToken)
     {
-        var repository = _unitOfWork.GetRepository<CartItem>();
+        var product = await GetProductById(request.ProductId);
+        var cart = await GetOrCreateCart(cancellationToken);
+        var cartItem = CreateCartItem(product, request.Quantity);
 
-        // Retrieve the product from the database using the provided ProductId
-        var product = await _unitOfWork.GetRepository<Product>().GetByIdAsync(request.ProductId);
+        cart.Items.Add(cartItem);
+        await _unitOfWork.SaveChangesAsync();
+
+        return cartItem.Id;
+    }
+
+    private async Task<Product> GetProductById(int productId)
+    {
+        var product = await _unitOfWork.GetRepository<Product>().GetByIdAsync(productId);
 
         if (product == null)
         {
-            // Handle the scenario where the product doesn't exist
-            throw new NotFoundException("Product not found.", request.ProductId);
+            throw new NotFoundException("Product not found.", productId);
         }
 
+        return product;
+    }
+
+    private async Task<Cart> GetOrCreateCart(CancellationToken cancellationToken)
+    {
+        var userId = Guid.Parse(_userContextService.GetUserContext().UserId);
         var cartRepository = _unitOfWork.GetRepository<Cart>();
-        var cartSpec = new CartByUserIdSpecification(request.UserId);
+        var cartSpec = new CartByUserIdSpecification(userId);
         var cart = await cartRepository.GetBySpecAsync(cartSpec, cancellationToken);
 
         if (cart == null)
         {
-            // Create a new cart for the user if it doesn't exist
-            cart = new Cart(request.UserId);
+            cart = CreateCart(userId);
             await cartRepository.AddAsync(cart, cancellationToken);
         }
 
-        // Create a new cart item and map the data from the request
-        var cartItem = new CartItem
+        return cart;
+    }
+
+    private CartItem CreateCartItem(Product product, int quantity)
+    {
+        return new CartItem
         {
             ProductId = product.Id,
             ProductName = product.Name,
-            Quantity = request.Quantity,
+            Quantity = quantity,
             Price = product.Price
         };
+    }
 
-        cart.Items.Add(cartItem);
-
-        await _unitOfWork.SaveChangesAsync();
-
-        return cartItem.Id;
+    private Cart CreateCart(Guid userId)
+    {
+        return new Cart(userId);
     }
 }
