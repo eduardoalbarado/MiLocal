04aa082319eefdbb3a4a15a7fdb597ccfc4330c8
Eduardo <eduardoalbarado@gmail.com>
2023-05-29 22:36:00 -0300

    Rename GeneraProfile.cs to GeneralProfile.cs

--- Application/Mappings/GeneralProfile.cs
+++ Application/Mappings/GeneralProfile.cs
@@ -1,18 +1,23 @@
 ﻿using Application.Common.Models;
 using Application.Features.Carts.Commands.AddToCart;
+using Application.Features.Checkout.Commands.CreateOrder;
 using AutoMapper;
 using Domain.Entities;
 
 namespace Application.Mappings;
-public class GeneraProfile : Profile
+public class GeneralProfile : Profile
 {
-    public GeneraProfile()
+    public GeneralProfile()
     {
         #region Commands
         CreateMap<AddToCartDto, AddToCartCommand>();
+        CreateMap<AddToCartDto, AddToCartCommand>();
+        CreateMap<CartItem, OrderItemDto>();
+        CreateMap<OrderItemDto, OrderItem>();
         #endregion
         #region Queries
         CreateMap<Product, ProductDto>();
+        CreateMap<Order, OrderDto>();
         #endregion
     }
 }
