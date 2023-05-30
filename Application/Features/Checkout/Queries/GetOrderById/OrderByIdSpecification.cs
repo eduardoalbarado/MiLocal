239a10a76e731bbd71d8041c5a5b9c3039b934d6
Eduardo <eduardoalbarado@gmail.com>
2023-05-29 22:35:42 -0300

    Implement GetOrderById query

---
+++ Application/Features/Checkout/Queries/GetOrderById/OrderByIdSpecification.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using Ardalis.Specification;
+
+namespace Application.Features.Checkout.Queries.GetOrderById;
+public class OrderByIdSpecification : Specification<Order>
+{
+    public OrderByIdSpecification(Guid userId, int orderId)
+    {
+        Query.Where(order => order.UserId == userId && order.Id == orderId);
+    }
+}
