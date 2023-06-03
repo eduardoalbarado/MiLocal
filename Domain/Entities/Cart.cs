8d300e57cb594bf70eedc31f3057d7f96ad4c8ba
Eduardo <eduardoalbarado@gmail.com>
2023-06-03 01:07:18 -0300

    refactor: Update Cart and CartItem entities - Update Cart.cs:   - Add virtual keyword to Total and TotalDiscount properties for potential override in derived classes.
    - Update CartItem.cs:
      - Add Discount property for applying discounts to cart items.
      - Update Subtotal and DiscountedSubtotal properties to consider the applied discount.

--- Domain/Entities/Cart.cs
+++ Domain/Entities/Cart.cs
@@ -6,8 +6,14 @@ public class Cart : BaseEntity
     public Guid UserId { get; set; }
     public List<CartItem> Items { get; set; }
     public decimal TotalPrice { get; set; }
-    // Additional properties and methods for the shopping cart
-
+    public virtual decimal Total
+    {
+        get { return Items.Sum(item => item.DiscountedSubtotal); }
+    }
+    public virtual decimal TotalDiscount
+    {
+        get { return Items.Sum(item => item.Subtotal - item.DiscountedSubtotal); }
+    }
     public Cart(Guid userId)
     {
         UserId = userId;
--- Domain/Entities/CartItem.cs
+++ Domain/Entities/CartItem.cs
@@ -1,10 +1,21 @@
+using Domain.Common;
+
 namespace Domain.Entities;
-public class CartItem
+public class CartItem : BaseEntity
 {
-    public int Id { get; set; }
     public int ProductId { get; set; }
     public string ProductName { get; set; }
     public int Quantity { get; set; }
+    public string AdditionalInfo { get; set; }
+
     public decimal Price { get; set; }
-    // Additional properties and methods for the cart item
+    public decimal Discount { get; set; }
+    public virtual decimal Subtotal
+    {
+        get { return Price * Quantity; }
+    }
+    public decimal DiscountedSubtotal
+    {
+        get { return Subtotal * (1 - Discount); }
+    }
 }
