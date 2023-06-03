0c158dee1d7e25c1daff87d643e21efd1385cc81
Eduardo <eduardoalbarado@gmail.com>
2023-06-03 20:30:24 -0300

    Refactor infrastructure services - Deleted unused DateTimeService class - Deleted unused EmailService class - Modified EmailService class in Infrastructure/Services directory

--- Infrastructure/Service/DateTimeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.Interfaces;
-
-namespace Infrastructure.Service;
-public class DateTimeService : IDateTime
-{
-    public DateTime Now => DateTime.Now;
-
-    public DateTime UtcNow => DateTime.UtcNow;
-}
--- Infrastructure/Services/EmailService.cs
+++ Infrastructure/Services/EmailService.cs
@@ -1,2 +1,27 @@
-// TODO: Implement the EmailService class for sending order-related emails.
+﻿using Application.Common.Models;
+using Application.Interfaces;
+using Ardalis.GuardClauses;
+using Microsoft.Extensions.Logging;
 
+namespace Infrastructure.Services;
+public class EmailService : IEmailService
+{
+    private readonly ILogger<EmailService> logger;
+    public EmailService(ILogger<EmailService> logger)
+    {
+        this.logger = logger;
+    }
+
+    public Task SendAsync(EmailMessageDto message)
+    {
+        Guard.Against.Null(message, nameof(message));
+
+        logger.LogCritical("Sending email");
+
+        // Likely use something like SendGrid here.
+
+        // Consider sending the email message to a queue so that retries can be handled.
+
+        return Task.CompletedTask;
+    }
+}
