a47685f99ea086284cfad81d308fe7c54bcd6407
Eduardo <eduardoalbarado@gmail.com>
2023-06-04 00:16:14 -0300

    Add Infrastructure Service

---
+++ Infrastructure/Services/DateTimeService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Interfaces;
+
+namespace Infrastructure.Service;
+public class DateTimeService : IDateTime
+{
+    public DateTime Now => DateTime.Now;
+
+    public DateTime UtcNow => DateTime.UtcNow;
+}
