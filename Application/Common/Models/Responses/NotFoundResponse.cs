28eb02ce5bc98d147952f91eb13f10ec175622bf
Eduardo <eduardoalbarado@gmail.com>
2023-06-05 23:46:08 -0300

    Add nre model responses

---
+++ Application/Common/Models/Responses/NotFoundResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Common.Models.Responses;
+public class NotFoundResponse
+{
+    public string Error { get; set; } = "NotFound";
+    public string Message { get; set; } = "The requested resource was not found.";
+    public string ResourceId { get; set; } = string.Empty;
+}
---
+++ Application/Common/Models/Responses/ValidationErrorResponse.cs
@@ -0,0 +1,34 @@
+﻿namespace Application.Common.Models.Responses;
+public class ValidationErrorResponse
+{
+    public string Error { get; set; } = "ValidationError";
+    public string Message { get; set; } = "One or more validation errors occurred.";
+    public List<ValidationError> ValidationErrors { get; set; }
+    public ValidationErrorResponse(List<ValidationError> validationErrors)
+    {
+        ValidationErrors = validationErrors;
+    }
+    public ValidationErrorResponse(string message, List<ValidationError> validationErrors)
+    {
+        Message = message;
+        ValidationErrors = validationErrors;
+    }
+
+}
+
+public class ValidationError
+{
+    /// <summary>
+    /// The name of the property.
+    /// </summary>
+    public string PropertyName { get; set; }
+
+    /// <summary>
+    /// The error message
+    /// </summary>
+    public string ErrorMessage { get; set; }
+
+    /// <summary>
+    /// The property value that caused the failure.
+    /// </summary>
+}
