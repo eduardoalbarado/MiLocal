00a3c82236386bf29cae3886e6d18a7b32a1df5f
Eduardo <eduardoalbarado@gmail.com>
2023-06-07 23:55:16 -0300

    Add ProductImage entity and update Product entity - Added ProductImage entity to store product images data - Updated Product entity to include a collection of images

---
+++ Domain/Entities/ProductImage.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+public class ProductImage : BaseEntity
+{
+    public int ProductId { get; set; }
+    public Guid Identifier { get; set; }
+    public bool IsMainImage { get; set; }
+
+    public virtual Product Product { get; set; }
+}
