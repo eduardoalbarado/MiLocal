b37e1dd5baf6f4739453eaa2a70e8b7d5dbfb71d
Eduardo <eduardoalbarado@gmail.com>
2024-03-10 14:30:47 -0300

    fix: Include Cart Items in CartByUserIdSpecification and add ItemProfile
    Enhanced CartByUserIdSpecification to include Cart Items during the query. Also, introduced ItemProfile for AutoMapper to handle mapping from CartItem to CartItemDto. This resolves issues related to Item loading in the Cart and AutoMapper configuration errors.

---
+++ Application/MappingProfiles/ItemProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.Features.Carts.Queries.GetCart;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfiles1
+{
+    public class ItemProfile : Profile
+    {
+        public ItemProfile()
+        {
+            CreateMap<CartItem, CartItemDto>();
+        }
+    }
+}
