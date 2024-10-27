a1a15c2c7902d709c4a601fa8eaa62fac735b7a8
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-10-27 14:56:49 -0300

    feat: Add HttpRequestDataExtensions for creating JSON responses

---
+++ Presentation/FunctionAppApi/Extensions/HttpRequestDataExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Net.Http.Headers;
+using System.Net.Mime;
+
+namespace FunctionAppApi.Extensions;
+
+public static class HttpRequestDataExtensions
+{
+    public static async Task<HttpResponseData> CreateHttpResponseAsync(
+        this HttpRequestData request, object data, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+        var response = request.CreateResponse(statusCode);
+        response.Headers.Add(HeaderNames.ContentType, MediaTypeNames.Application.Json);
+        await response.WriteAsJsonAsync(data);
+
+        return response;
+    }
+}
