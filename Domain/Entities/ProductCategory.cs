12488d1fba4f06552a1b0667223ad6cd7c39cd35
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-11-01 14:43:08 -0300

    Updated structure and namespace in Domain entities

--- Domain/Entities/ProductCategory.cs
+++ Domain/Entities/ProductCategory.cs
@@ -1,10 +1,9 @@
-﻿namespace Domain.Entities
+﻿namespace Domain.Entities;
+
+public class ProductCategory
 {
-    public class ProductCategory
-    {
-        public int ProductId { get; set; }
-        public virtual Product Product { get; set; }
-        public int CategoryId { get; set; }
-        public virtual Category Category { get; set; }
-    }
+    public int ProductId { get; set; }
+    public virtual Product Product { get; set; }
+    public int CategoryId { get; set; }
+    public virtual Category Category { get; set; }
 }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,14 +1,13 @@
-﻿namespace Domain.Entities
+﻿namespace Domain.Entities;
+
+public class User
 {
-    public class User
-    {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string? B2CUserId { get; set; }
-        public string? GmailId { get; set; }
-        public string? FacebookId { get; set; }
-        public ICollection<Cart> Carts { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string? B2CUserId { get; set; }
+    public string? GmailId { get; set; }
+    public string? FacebookId { get; set; }
+    public ICollection<Cart> Carts { get; set; }
 }
