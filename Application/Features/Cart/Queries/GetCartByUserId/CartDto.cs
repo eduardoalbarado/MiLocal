97a483cebefbe7688fe113c9856754ddf9f6057a
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-11-01 14:52:47 -0300

    refactor(cart): Remove deprecated cart query files to streamline functionality

--- Application/Features/Cart/Queries/GetCartByUserId/CartDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Features.Carts.Queries.GetCartByUserId;
-public class CartDto
-{
-    public int UserId { get; set; }
-    public List<CartItemDto> Items { get; set; }
-    public decimal TotalPrice { get; set; }
-}
--- Application/Features/Cart/Queries/GetCartByUserId/CartItemDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.Features.Carts.Queries.GetCartByUserId;
-public class CartItemDto
-{
-    public int ProductId { get; set; }
-    public string ProductName { get; set; }
-    public decimal Price { get; set; }
-    public int Quantity { get; set; }
-}
