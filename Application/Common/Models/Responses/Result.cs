9c1e546d0019db5dc5d86fcc75834c633b306fc7
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-10-31 23:56:08 -0300

    Refactor error response models for improved clarity

--- Application/Common/Models/Responses/Result.cs
+++ Application/Common/Models/Responses/Result.cs
@@ -1,24 +1,16 @@
 ﻿namespace Application.Common.Models.Responses;
 public class Result<T>
 {
-    public bool IsSuccess { get; }
+    public bool IsSuccess { get; } = true;
     public T Data { get; }
-    public string ErrorMessage { get; }
 
-    private Result(bool isSuccess, T data, string errorMessage)
+    private Result(T data)
     {
-        IsSuccess = isSuccess;
         Data = data;
-        ErrorMessage = errorMessage;
     }
 
     public static Result<T> Success(T data)
     {
-        return new Result<T>(true, data, null);
-    }
-
-    public static Result<T> Failure(string errorMessage, T data = default)
-    {
-        return new Result<T>(false, data, errorMessage);
+        return new Result<T>(data);
     }
 }
--- Application/Common/Models/Responses/ValidationErrorResponse.cs
+++ Application/Common/Models/Responses/ValidationErrorResponse.cs
@@ -1,34 +1,11 @@
 ﻿namespace Application.Common.Models.Responses;
-public class ValidationErrorResponse
+public class ValidationErrorResponse : ErrorResponse
 {
-    public string Error { get; set; } = "ValidationError";
-    public string Message { get; set; } = "One or more validation errors occurred.";
-    public List<ValidationError> ValidationErrors { get; set; }
-    public ValidationErrorResponse(List<ValidationError> validationErrors)
+    public Dictionary<string, string[]> Errors { get; set; }
+
+    public ValidationErrorResponse(string title, Dictionary<string, string[]> errors)
+        : base(title, "Validation failed for one or more properties.")
     {
-        ValidationErrors = validationErrors;
+        Errors = errors;
     }
-    public ValidationErrorResponse(string message, List<ValidationError> validationErrors)
-    {
-        Message = message;
-        ValidationErrors = validationErrors;
-    }
-
-}
-
-public class ValidationError
-{
-    /// <summary>
-    /// The name of the property.
-    /// </summary>
-    public string PropertyName { get; set; }
-
-    /// <summary>
-    /// The error message
-    /// </summary>
-    public string ErrorMessage { get; set; }
-
-    /// <summary>
-    /// The property value that caused the failure.
-    /// </summary>
 }
