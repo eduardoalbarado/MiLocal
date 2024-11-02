fba892b8dd1f6d28402cdd2778e41048c4936bc0
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-11-01 22:29:05 -0300

    refactor(user): Rename GetUserByB2CUserIdQuery to GetUserByIdQuery and update handler for ID-based user retrieval

--- Application/Features/Users/Queries/GetUserByB2CUserId/GetUserByIdQuery.cs
+++ Application/Features/Users/Queries/GetUserByB2CUserId/GetUserByIdQuery.cs
@@ -4,29 +4,29 @@ using MediatR;
 
 namespace Application.Features.Users.Queries.GetUserByB2CUserId;
 
-public class GetUserByB2CUserIdQuery : IRequest<bool>
+public class GetUserByIdQuery : IRequest<bool>
 {
     public string B2CUserId { get; set; }
 
-    public GetUserByB2CUserIdQuery(string b2cUserId)
+    public GetUserByIdQuery(string b2cUserId)
     {
         B2CUserId = b2cUserId;
     }
 }
 
-public class GetUserByB2CUserIdQueryHandler : IRequestHandler<GetUserByB2CUserIdQuery, bool>
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, bool>
 {
     private readonly IUnitOfWork _unitOfWork;
 
-    public GetUserByB2CUserIdQueryHandler(IUnitOfWork unitOfWork)
+    public GetUserByIdQueryHandler(IUnitOfWork unitOfWork)
     {
         _unitOfWork = unitOfWork;
     }
 
-    public async Task<bool> Handle(GetUserByB2CUserIdQuery request, CancellationToken cancellationToken)
+    public async Task<bool> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
     {
         var userRepository = _unitOfWork.GetRepository<User>();
-        var userSpec = new UserByB2CUserIdSpec(request.B2CUserId);
+        var userSpec = new UserByIdSpec(request.B2CUserId);
         var existingUser = await userRepository.FirstOrDefaultAsync(userSpec, cancellationToken);
 
         return existingUser != null;
