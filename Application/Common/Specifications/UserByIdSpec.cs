30cb3946967a5eccf862d31853727699f871707f
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-11-01 22:08:26 -0300

    refactor(specifications): Rename and modify UserByB2CUserIdSpec to UserByIdSpec, updating query to use Id instead of B2CUserId

---
+++ Application/Common/Specifications/UserByIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using Domain.Entities;
+
+namespace Application.Common.Specifications;
+
+public class UserByIdSpec : Specification<User>
+{
+    public UserByIdSpec(string b2cUserId)
+    {
+        Query.Where(user => user.Id == Guid.Parse(b2cUserId));
+    }
+}
