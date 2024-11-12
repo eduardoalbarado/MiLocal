db5e7c40834dc47e57e674ceb9782d065f31a948
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-11-12 00:08:07 -0300

    feat: add `StartCheckoutRequestDto` with shipping, payment, and location details
    - Standardizes checkout request structure.

---
+++ Application/Common/Models/Checkout/StartCheckoutRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using Domain.ValueObjects;
+
+namespace Application.Common.Models.Checkout;
+
+public class StartCheckoutRequestDto
+{
+    public string ShippingMethod { get; set; }
+    public string ShippingAddress { get; set; }
+    public string PaymentMethod { get; set; }
+    public Location Location { get; set; }
+}
