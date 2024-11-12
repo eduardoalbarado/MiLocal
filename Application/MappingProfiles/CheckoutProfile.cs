b35e95a355323c97278eb9afe332251733694e2d
Eduardo Albarado <eduardoalbarado@hotmail.com>
2024-11-12 00:08:55 -0300

    feat: add AutoMapper profiles for checkout and order mapping
    - `CheckoutProfile`: `StartCheckoutRequestDto` to `StartCheckoutCommand`
    - `OrderProfile`: `CartItem` to `OrderItem`

---
+++ Application/MappingProfiles/CheckoutProfile.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Models.Checkout;
+using Application.Features.Checkout.Commands.StartCheckout;
+using AutoMapper;
+
+namespace Application.MappingProfiles;
+
+public class CheckoutProfile : Profile
+{
+    public CheckoutProfile()
+    {
+        CreateMap<StartCheckoutRequestDto, StartCheckoutCommand>();
+    }
+}
---
+++ Application/MappingProfiles/OrderProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfiles;
+
+public class OrderProfile : Profile
+{
+    public OrderProfile()
+    {
+        CreateMap<CartItem, OrderItem>();
+    }
+}
